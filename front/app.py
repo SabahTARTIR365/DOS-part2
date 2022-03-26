9d210c1250911a6e9c93b8c1a94dea4024abe336
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-03-26 15:51:31 +0300

    add search opearation to front server

--- front/app.py
+++ front/app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 def hello():
   return "Hello World!"
 
+
+# Search opearation
+# return the books information( Item Number (ID),title) according to a  given topic
+# this reqest send to the catalog server
+@app.route('/search/<topic>', methods=['Get'])
+def search(topic):
+    response = requests.get("http://192.168.1.135:5000/search/" + topic)
+    return response.content
+
+
+
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True, port=3500)
