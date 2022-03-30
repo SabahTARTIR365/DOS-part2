80d6583a5d2bab2183fc078ceb74f7d81e6456a7
rawan <rawanabodiak12@gmail.com>
2022-03-30 20:14:20 +0300

    add search(topic) to catalog server

--- catalog/venv/app.py
+++ catalog/venv/app.py
@@ -2,16 +2,39 @@ from flask import Flask
 from flask_restful import Resource, Api
 import sqlite3
 
-#connect to db 
-conn = sqlite3.connect('store.db')
 app = Flask(__name__)
 api = Api(app)
+
+
+def getFromDB(stetment):
+    #database connection
+    sqliteConnection = sqlite3.connect('store.db')
+    cursor = sqliteConnection.cursor()
+    #execute sql quary
+    count  = cursor.execute(stetment)
+    rows = (cursor.fetchall())
+    sqliteConnection.commit()
+    cursor.close()
+    sqliteConnection.close()
+    return {'result ':rows}
+
+
+
+class searchTopic(Resource):
+    def get(self,topic):
+       topic1 = topic.replace("%20"," ")
+       
+       query='select title,quantity from books where topic= "' + topic1+'"'
+       return getFromDB(query)
+       
+
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(searchTopic, '/search/<topic>')
 
 if __name__ == '__main__':
     app.run(debug=True)
