f11ea13772a68505e57ed9035d3f9e6d13b89632
rawan <rawanabodiak12@gmail.com>
2022-03-30 14:52:27 +0300

    insert records in database

--- catalog/venv/DbCreation.py
+++ catalog/venv/DbCreation.py
@@ -13,6 +13,10 @@ c.execute("""CREATE TABLE books
           topic text,
           quantity INTEGER,
           cost REAL)""")
-          
+         
+c.execute("INSERT INTO books VALUES(1 ,'How to get a good grade in DOS in 40 minutes a day' , 'distributed system' , 300 ,20 )")
+c.execute("INSERT INTO books VALUES(2 ,'RBCs for Noobs' , 'distributed system' , 100 ,25 )")  
+c.execute("INSERT INTO books VALUES(3 ,'HXen and the Art of Surviving Undergraduate School' , 'undergraduate school' , 600 ,60 )") 
+c.execute("INSERT INTO books VALUES(4 ,'Cooking for the Impatent Undergrad' , 'undergraduate school' , 600 ,100 )")         
 conn.commit()          
 
