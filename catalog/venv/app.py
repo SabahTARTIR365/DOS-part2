d32fca7c89a922bf77be3298d09fc6f130c3aa1b
rawan <rawanabodiak12@gmail.com>
2022-03-30 22:25:31 +0300

    info service in catalog

--- catalog/venv/app.py
+++ catalog/venv/app.py
@@ -16,25 +16,50 @@ def getFromDB(stetment):
     sqliteConnection.commit()
     cursor.close()
     sqliteConnection.close()
-    return {'result ':rows}
+    finalResult =[]
+    return rows;
+    
 
-
-
+#search(topic)
 class searchTopic(Resource):
     def get(self,topic):
        topic1 = topic.replace("%20"," ")
-       
+
        query='select title,quantity from books where topic= "' + topic1+'"'
-       return getFromDB(query)
-       
+       rows = getFromDB(query)
+       if(len(rows)==0) :
+          return{'result ': 'unkown topic'}
+       finalResult = []
+       for i in rows :
+          dic =dict(title = i[0],quantity = i[1])
+          finalResult.append(dic)
+       return {'result ':finalResult}
 
 
+
+#search according to item number and return all info 
+class info(Resource):
+    def get(self,number):
+
+       query='select * from books where item_number='+ str(number)
+       rows =getFromDB(query)
+       if(len(rows)==0) :
+          return{'result ': 'no results for your query, please be sure you  entered valid item number '}
+           
+       finalResult = []
+       for i in rows :
+          dic =dict(item_number =i[0],title = i[1],topic= i[2] , quantity =
+           i[3] , cost = i[4])
+          finalResult.append(dic)
+       return {'result ':finalResult}
+              
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(searchTopic, '/search/<topic>')
+api.add_resource(info, '/info/<int:number>')
 
 if __name__ == '__main__':
     app.run(debug=True)
