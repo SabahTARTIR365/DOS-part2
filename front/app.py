0bc7a57a97e9834543e3fed9db7a65bf50f6a2db
R-abodyak <rawanabodiak12@gmail.com>
2022-05-04 18:29:55 +0300

    depends on different port but same address

--- front/app.py
+++ front/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask import request
 import requests
 app = Flask(__name__)
-orderIpAddress1= "192.168.1.14"
-orderIpAddress2= "192.168.1.15"
-catalogIpAddress1="192.168.1.70"
-catalogIpAddress2="192.168.1.71"
+orderIpAddress= "192.168.1.14"
+catalogIpAddress="192.168.1.70"
+
+Port1 = 5000
+Port2 = 4000
 
 catalog_counter = 1 
 order_counter = 1 
@@ -13,23 +14,23 @@ def catalog_round_robin():
     global catalog_counter
     #round robin between catalog servers 
     if(catalog_counter ==1):
-      catalogIpAddress=catalogIpAddress1
+      port=Port1
       catalog_counter = 2
     if(catalog_counter ==2):
-      catalogIpAddress=catalogIpAddress2
+      port=Port2
       catalog_counter = 1
-    return  catalogIpAddress
+    return  port
 
 def order_round_robin():
     global order_counter
     #round robin between catalog servers 
     if(order_counter ==1):
-      orderIpAddress=orderIpAddress1
+      port=Port1
       order_counter = 2
     if(order_counter ==2):
-      orderIpAddress=orderIpAddress2
+      port=Port2
       order_counter = 1  
-    return  orderIpAddress
+    return  port
      
 @app.route("/")
 def hello():
@@ -41,34 +42,35 @@ def hello():
 # this reqest send to the catalog server
 @app.route('/search/<topic>', methods=['Get'])
 def search(topic):
-    catalogIpAddress=catalog_round_robin()
-    response = requests.get("http://"+catalogIpAddress+":5000/search/" + topic)
+    port=catalog_round_robin()
+    response = requests.get("http://"+catalogIpAddress+":"+port+"/search/" + topic)
     return response.content
-
+  
 # info opearation
 # return  information about specific book according to given ID
 # this req send to catalog server 
 @app.route('/info/<int:id>', methods=['Get'])
 def information_id(id):
-    response = requests.get("http://"+catalogIpAddress+":5000/info/" + str(id))
+    port=catalog_round_robin()
+    response = requests.get("http://"+catalogIpAddress+":"+port+"/info/" + str(id))
     return response.content
 
 # purchase opearation
 #this post req will be sent to ORDER server in order to purchase specific book
 @app.route('/purchase/<int:id>', methods=['Post'])
 def purchase(id):
-    orderIpAddress=order_round_robin()
-    response = requests.post("http://"+orderIpAddress+":5000/purchase/" + str(id))
+    port=order_round_robin()
+    response = requests.post("http://"+orderIpAddress+":"+port+"/purchase/" + str(id))
     return response.content
 
 # this req will be used from the internal system by the admin in order to
 #update price of specific book - it will send to catalog server
 @app.route('/update_price/<int:id>', methods=['Put'])
 def update_price(id):
-    catalogIpAddress=catalog_round_robin()
+    port=catalog_round_robin()
     price = request.json['price']
-    response = requests.put("http://"+catalogIpAddress+":5000/update_price/" + str(id), {'price': price})
+    response = requests.put("http://"+catalogIpAddress+":"+port+"/update_price/" + str(id), {'price': price})
     return response.content
 
 if __name__ == "__main__":
-  app.run(debug=True, port=3500)
+  app.run(debug=True)
