393a9647fd61df75604f4015fe27edff0428eb2c
R-abodyak <rawanabodiak12@gmail.com>
2022-05-05 00:10:27 +0300

    fix add new books to DB script

--- catalog2/venv/addNewBooksScript.py
+++ catalog2/venv/addNewBooksScript.py
@@ -8,3 +8,4 @@ c = conn.cursor();
 c.execute("INSERT INTO books VALUES(5 ,'. How to finish Project 3 on time' , 'spring' , 300 ,150 )")
 c.execute("INSERT INTO books VALUES(6 ,'Why theory classes are so hard' ,  'spring' , 100 ,125 )")  
 c.execute("INSERT INTO books VALUES(7 ,'Spring in the Pioneer Valley' , 'spring' , 600 ,50 )") 
+conn.commit()          
