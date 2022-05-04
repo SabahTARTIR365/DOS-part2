0b8307f2543a4f78c062d74b6f8e933b5c4de898
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-05-04 19:16:36 +0300

    add round_robin functions

--- front/app.py
+++ front/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 orderIpAddress= "192.168.1.14"
 catalogIpAddress="192.168.1.70"
 
+Port1 = 5000
+Port2 = 4000
+
+catalog_counter = 1 
+order_counter   = 1 
 
 class LRUCache:
   def __init__(self, size):
@@ -39,20 +44,41 @@ class LRUCache:
   def show_entries(self):
     print(self.lru_cache)
 
-# Create an LRU Cache with a size of 3
+# Create an LRU Cache with a size of 5
 cache = LRUCache(5)
 
+def catalog_round_robin():
+    global catalog_counter
+    #round robin between catalog servers 
+    if(catalog_counter ==1):
+      port=Port1
+      catalog_counter = 2
+    if(catalog_counter ==2):
+      port=Port2
+      catalog_counter = 1
+    return  port
+
+def order_round_robin():
+    global order_counter
+    #round robin between catalog servers 
+    if(order_counter ==1):
+      port=Port1
+      order_counter = 2
+    if(order_counter ==2):
+      port=Port2
+      order_counter = 1  
+    return  port
+  
 @app.route("/")
 def hello():
-  return "Hello World!"
-
-
+    return "Hello World!"
 # Search opearation
 # return the books information( Item Number (ID),title) according to a  given topic
 # this reqest send to the catalog server
 @app.route('/search/<topic>', methods=['Get'])
 def search(topic):
-    response = requests.get("http://"+catalogIpAddress+":5000/search/" + topic)
+    port=catalog_round_robin()
+    response = requests.get("http://"+catalogIpAddress+":"+port+"/search/" + topic)
     return response.content
 
 # info opearation
@@ -60,23 +86,27 @@ def search(topic):
 # this req send to catalog server 
 @app.route('/info/<int:id>', methods=['Get'])
 def information_id(id):
-    response = requests.get("http://"+catalogIpAddress+":5000/info/" + str(id))
+    port=catalog_round_robin()
+    response = requests.get("http://"+catalogIpAddress+":"+port+"/info/" + str(id))
     return response.content
 
 # purchase opearation
 #this post req will be sent to ORDER server in order to purchase specific book
 @app.route('/purchase/<int:id>', methods=['Post'])
 def purchase(id):
-    response = requests.post("http://"+orderIpAddress+":5000/purchase/" + str(id))
+    port=order_round_robin()
+    response = requests.post("http://"+orderIpAddress+":"+port+"/purchase/" + str(id))
     return response.content
 
 # this req will be used from the internal system by the admin in order to
 #update price of specific book - it will send to catalog server
 @app.route('/update_price/<int:id>', methods=['Put'])
 def update_price(id):
+    port=catalog_round_robin()
     price = request.json['price']
-    response = requests.put("http://"+catalogIpAddress+":5000/update_price/" + str(id), {'price': price})
+    response = requests.put("http://"+catalogIpAddress+":"+port+"/update_price/" + str(id), {'price': price})
     return response.content
 
 if __name__ == "__main__":
-  app.run(debug=True, port=3500)
+  app.run(debug=True)
+
